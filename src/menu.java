aa769a527abed10633740305d7a848aa434d98d1
alan-hash <62110525+alan-hash@users.noreply.github.com>
2022-07-09 00:38:36 -0500

    EWWW
    EEEEEEEE

--- src/menu.java
+++ src/menu.java
@@ -46,7 +46,7 @@ public class menu extends javax.swing.JFrame {
         jLabel1.setText("MENU PRINCIPAL");
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 20, -1, -1));
 
-        jButton1.setText("SOPA");
+        jButton1.setText("SSSS");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
